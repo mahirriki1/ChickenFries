62dc7eb3de532e03fbb6e947d713f5de96c7d1c2
mahirriki1 <mriki30@stuy.edu>
2022-09-29 22:02:03 -0400

    completed krewes.py

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,29 +1,33 @@
 import random
-from tkinter import N
-
 
 with open('krewes.txt', 'r') as f:
     lines = f.readline()
 krewes_list = lines.split("@@@")
 
-krewes = {2: [],
-          7: [],
-          8: []
-          }
+krewes = {}
           
 for i in krewes_list:
     person = i.split("$$$")
-    if (not int(person[0]) in krewes.keys()):
-        krewes[int(person[0])] = []
+    pd_num = int(person[0])
+    if (not pd_num in krewes.keys()):
+        krewes[pd_num] = []
     list = [person[1], person[2]]
-    krewes[int(person[0])].append(list)
+    krewes[pd_num].append(list)
 
-def random_devo(krewes):
+def random_devo():
 # printing both the devo's name, period, and ducky
-    period = random.choice(krewes.keys())
-    person_and_ducky = random.choice(list(krewes[period]))
+# list is bugging out, so we need this roundabout way of 
+    counter = 0
+    period = -1
+    index = random.randrange(0, len(krewes.keys()))
+    for i in krewes.keys():
+        if (counter == index):
+            period = i
+        counter += 1
+    person_and_ducky = random.choice(krewes[period])
     name = person_and_ducky[0]
     ducky = person_and_ducky[1]
-    print(period + ", " + name + ", " + ducky)
-    
-print(random_devo(krewes))
+    print("Period " + str(period) + ": " + name + " with ducky " + ducky)
+
+
+random_devo()
